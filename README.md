982d89bff0e78795b30650e995bfa5059d07f766
okeatama <okeatama@gmail.com>
2025-06-21 15:13:08 +1000

    created updated_mass_readings.json and the program to make it

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # ICFSlides
 Web scraper to automate mass slides
+
+Included is scraped bible (New Jerusalem Bible) from https://www.catholic.org/bible/book.php, can be found in bible/bible.json. 0.json - 72.json are each books in the bible. To access the bible's verses, use python
+
+```
+import json
+from tutotial.declarations import books
+
+with open("bible/bible.json", "r") as file:
+    bible = json.load(file)
+book_name = "Genesis" # change to whatever the name of the book is
+book_number = books.index(book_name)
+chapter = 1
+
+# programmer indexing vs normal people indexing
+verse_start = 1 - 1 # always minus 1
+verse_end = 10 - 1 # always minus 1
+verse = 5 - 1 # always minus 1
+
+bible[book_number][chapter][verse_start:verse_end]
+bible[book_number][chapter][verse]
+```
+
+To scrape:
+```
+cd tutorial
+./run_crawler.bat
+```
+
+To compile all the books copy the 0.json to 72.json to bible folder, then run create_bible.py `py ./create_bible.py`
+
+To get sunday mass readings, run `./gen_readings.bat`
+
+ICF uses Universalis, which I believe use NJB, but mass_readings.json uses another version, so update_mass_reading.py will update it to use NJB. Need to change mass_readings.json a bit, if there are any "or" readings (even though they already have 2 different readings, they didn't need the or), remove them and replace accordingly
--- update_mass_readings.py
+++ update_mass_readings.py
@@ -5,6 +5,110 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 with open("mass_readings.json", "r") as file:
-    data = json.load(file)
+    readings = json.load(file)
 
-base_url = "https://www.catholic.org/bible/book.php"
+with open("bible/bible.json", "r") as file:
+    bible = json.load(file)
+
+accepted_types = ["READING", "GOSPEL"]
+
+# best use debugger to play around with the json, very confusing otherwise
+for i, reading in enumerate(readings):
+    sections = reading["sections"]
+    if sections == []:
+        continue
+
+    for j, section in enumerate(sections):
+        if section["type"] not in accepted_types:
+            continue
+        
+        for k, r in enumerate(section["readings"]):
+
+            verses = r["verses"][0]
+            # in the form of {BOOK_NAME} {CHAPTER_NUM}:{VERSE_START}-{VERSE_END}
+
+            book_name = verses["book"]
+            book_num = books.index(book_name)
+
+            # remove book name, so it should now only be {CHAPTER_NUM}:{VERSE_START}-{VERSE_END}
+            verse_split = verses["text"][len(book_name) + 1:].strip().replace(";", ",")
+
+            whole_text = ""
+            # possibility of there being 2
+            for comma_split in verse_split.split(","):
+                # comma_split is in the form {CHAPTER_NUM}:{VERSE_START}-{VERSE_END}
+                # or just {VERSE_START}-{VERSE_END}, if so then use same chapter
+
+                if ":" not in comma_split:
+                    chapter = 0
+                
+                chapter_verse = comma_split.split(":")
+
+                if len(chapter_verse) != 1:
+                    # chapter given
+                    chapter = int(chapter_verse[0]) - 1
+
+                    if "-" not in chapter_verse[1]:
+                        verse = chapter_verse[1].strip()
+
+                        if not verse.isdigit():
+                            print(f"WARNING: there is alphabet in verse: {verse}")
+                            verse = verse[:-1]
+
+                        verse = int(verse)
+
+                        text = bible[book_num][chapter][verse]
+                        whole_text += text.strip()
+                    else:
+
+                        verse_start, verse_end = chapter_verse[1].split("-")
+                        verse_start = verse_start.strip()
+                        verse_end = verse_end.strip()
+
+                        if not verse_start.isdigit():
+                            print(f"WARNING: there is alphabet in verse_start, double check: {verse_start}")
+                            verse_start = verse_start[:-1]
+                        if not verse_end.isdigit():
+                            print(f"WARNING: there is alphabet in verse_end, double check: {verse_end}")
+                            verse_end = verse_end[:-1]
+
+                        verse_start = int(verse_start) - 1
+                        verse_end = int(verse_end) - 1
+
+                        text = "".join(bible[book_num][chapter][verse_start:verse_end])
+                        whole_text += text.strip()
+                else:
+                    # no chapter given
+                    if "-" not in comma_split:
+                        verse = comma_split.strip()
+
+                        if not verse.isdigit():
+                            print(f"WARNING: there is alphabet in verse: {verse}")
+                            verse = verse[:-1]
+                        
+                        verse = int(verse)
+
+                        text = bible[book_num][chapter][verse]
+                        whole_text += text.strip()
+                    else:
+                        verse_start, verse_end = comma_split.split("-")
+                        verse_start = verse_start.strip()
+                        verse_end = verse_end.strip()
+
+                        if not verse_start.isdigit():
+                            print(f"WARNING: there is alphabet in verse_start: {verse_start}")
+                            verse_start = verse_start[:-1]
+                        if not verse_end.isdigit():
+                            print(f"WARNING: there is alphabet in verse_end: {verse_end}")
+                            verse_end = verse_end[:-1]
+                        
+                        verse_start = int(verse_start) - 1
+                        verse_end = int(verse_end) - 1
+
+                        text = "".join(bible[book_num][chapter][verse_start:verse_end])
+                        whole_text += text.strip()
+
+            readings[i]["sections"][j]["readings"][k]["text"] = whole_text.strip()
+    
+with open("updated_mass_readings.json", "w") as file:
+    json.dump(readings, file, indent=4)
